022ddb37f485be4f1e70ecbe4e9d2d61eb30fc39
hensomxxxpipel <rachadrisyh@gmail.com>
2025-07-06 18:02:36 +0700

    Popular Product UI

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,40 @@
+import { shoe8 } from '../assets/images'
+import Button from '../components/Button'
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section
+    id="about-us"
+    className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-col">
+          <h2 className="font-palanquin text-4xl  capitalize font-bold lg:max-w-lg">
+            We Provide You
+            <span className="text-coral-red  "> Super </span>
+            <span className="text-coral-red  ">Quality </span>
+            Shoes
+          </h2>
+          <p className="mt-4 lg:max-w-lg info-text">Paling keren, gg, gacor, nyaman dan goattt. Di desain untuk berjalan di jembatan sirotalmustaqim. Dijamin lancar dan selamat sampai tujuan</p>
+
+          <p className='mt-6 lg:max-w-lg info-text'> Kami membuat semuanya hanya untuk Anda
+          </p>
+
+          <div className='mt-11'>
+            <Button label="View details"/>
+          </div>
+      </div>
+
+      <div className='flex-1 flex justify-center items-center'>
+        <img
+          src={shoe8}
+          alt='shoe8'
+          width={570}
+          height={522}
+          className='object-contain'
+        />
+
+      </div>
+    </section>
   )
 }
 
